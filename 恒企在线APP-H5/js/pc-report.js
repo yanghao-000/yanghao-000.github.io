df0b3a3d527817db714be68eb8a5c6818df99120
yanghao-000 <yanghao_000@126.com>
2016-09-06 23:57:10 +0800

    123
    123

--- 恒企在线APP-H5/js/pc-report.js
+++ 恒企在线APP-H5/js/pc-report.js
@@ -1,8 +1,13 @@
 $(function(){
-	drawInit(89,1000);
+	var passRate = 100;
+	var pArr = [70,50,30];
+	var lArr = ["06.01","06.02","06.03","06.04","06.05","06.06","06.07","06.08"];
+	var dArr = [20,40,30,10,90,20,50,40];
+	drawInit(passRate,1000,pArr,lArr,dArr);
 });
 
-function drawInit(inputNum,t){
+//表盘
+function drawInit(inputNum,t,arr,arr2,arr3){
 	var dial = document.getElementById("dial");
 	var ctx = dial.getContext("2d");
 	var w = dial.width;
@@ -11,8 +16,7 @@ function drawInit(inputNum,t){
 	var p = Math.PI;
 	var num = -1;
 	var deg;
-	var ter;
-	var time = t/inputNum;
+	var timer;
 	
 	var loadImg = ["images/pc-report/report-dot.png","images/pc-report/report-zhen.png"];
 	var imgNum = loadImg.length;
@@ -22,36 +26,41 @@ function drawInit(inputNum,t){
 		img.src=loadImg[i];
 		img.onload = function (){
 			k++;
-			if(k == imgNum) {
-				ter = setInterval(function(){
-					drawDial(inputNum);
-				},time);
+			if(k == imgNum){
+				setTimeout(function(){
+					numTiaoDong($(".show-total h1 span"),inputNum,t);
+					timer = setInterval(function(){
+						calDeg(inputNum);
+						drawDial(inputNum);
+					},t/inputNum);
+					$(".show-sub p").each(function(i){
+						barPercent($(this),arr[i],t);
+					});
+					chartLine(arr2,arr3);
+				},200);
 			};
 		};
 	};
 
 	function drawDial(n){
-		calDeg(n);
 		ctx.clearRect(0,0,w,h);
 		ctx.save();		
 		
-		ctx.translate(w/2,94);
+		ctx.translate(w/2,95);
 		ctx.save();
 		
 		ctx.beginPath();
 		ctx.rotate(p);
 		 //水平渐变值必须保持为0
-        var gr = ctx.createLinearGradient(0,0,0,124);
+        var gr = ctx.createLinearGradient(0,0,0,160);
         //添加颜色端点
 		gr.addColorStop(0,'rgba(255,255,255,0.0)');
         gr.addColorStop(.5,'rgba(255,255,255,1)');  
         gr.addColorStop(1,'rgba(255,255,255,0.0)');  
         ctx.strokeStyle = gr;
-//		ctx.fillStyle = gr;
 		ctx.lineWidth = 3;
 		ctx.arc(0,0,r,p,deg+p);
 		ctx.stroke();
-//		ctx.fill();
 		ctx.restore();
 		ctx.save();
 		
@@ -67,20 +76,70 @@ function drawInit(inputNum,t){
 		var dot = new Image();
 		dot.src = "images/pc-report/report-dot.png";
 		ctx.rotate(deg+p);
-		ctx.drawImage(dot,-97,-8,18,17);
+		ctx.drawImage(dot,-97,-9,18,17);
 		ctx.restore();
 		ctx.restore();
 		ctx.save();
-		
 	}
 	function calDeg(n){
 		num++;
-		if(num<n){
+		if(num<=n){
 			deg = p + num/100*p;
 		}else{
-			deg = p + num/100*p;
-			clearInterval(ter);
+			clearInterval(timer);
 			return false;
 		}	
 	}
+}
+
+//条形进度
+function barPercent(obj,per,t){
+	obj.animate({"width":per+"%"},t);
+	numTiaoDong(obj.parent().next().find("span"),per,t);
+}
+
+//折线图
+function chartLine(lArr,dArr){
+	var winW = $(window).outerWidth(true);
+	if(winW<=320){
+		$("#chartLine").attr("width",314).attr("height",160);
+	}else if(winW<=375){
+		$("#chartLine").attr("width",360).attr("height",190);
+	}else{
+		$("#chartLine").attr("width",390).attr("height",205);
+	}
+	
+	var scaleStepWidth = (Math.max.apply(null,dArr)+10)/5; //转化Y轴
+	
+	var ctx = document.getElementById("chartLine").getContext("2d");
+			
+	var strokeC=ctx.createLinearGradient(0,0,0,200);
+	strokeC.addColorStop(0,"rgba(89,225,219,1)");
+	strokeC.addColorStop(1,"rgba(51,160,248,1)"); 
+
+	var data = {
+		labels : lArr,
+		datasets : [
+			{
+				fillColor : "rgba(255,255,255,0)",
+				strokeColor : strokeC,
+				pointColor : "rgba(255,255,255,1)",
+				pointStrokeColor : "#289afb",
+				data : dArr
+			},
+		]
+	}
+	var options = {
+		animation : false,
+		scaleFontColor : "#333",
+		scaleGridLineColor : "#ddd",
+		animationSteps : 100,
+		pointDotRadius : 4,
+		pointDotStrokeWidth : 3,
+		scaleOverride : true,
+		scaleSteps : 5,
+		scaleStepWidth : scaleStepWidth,
+		scaleStartValue : 0
+	}
+	var myNewChart = new Chart(ctx).Line(data,options);
 }
