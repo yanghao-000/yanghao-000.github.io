4c8e1bfee097cbe7f1bcb7654ef913f5d5924234
yanghao-000 <yanghao_000@126.com>
2016-10-07 16:17:13 +0800

    123
    123

--- hqzxAPP-H5/js/com/com.js
+++ hqzxAPP-H5/js/com/com.js
@@ -282,9 +282,9 @@ function judgeAnd(){
 		$(".pass-body .head .w2 h1").css({"marginTop":"0.7em"});
 		
 		$(".pass-body .body .w1").each(function(){
-			$(this).find("p").eq(0).css({"marginTop":"0.4em"});
+			$(this).find("p").eq(0).css({"marginTop":"0.6em"});
 		});
-		$(".pass-body .head .w2.dif h1").css({"lineHeight":"3.6em"});
+		$(".pass-body .head .dif h1").css({"lineHeight":"3.6em","height":"3.6em"});
 	}
 }
 judgeAnd();
--- hqzxAPP-H5/js/pass.js
+++ hqzxAPP-H5/js/pass.js
@@ -1,4 +1,4 @@
 $(function(){
 	judgeAnd();
-//	$(".pass-body .head .w2.dif h1").css({"lineHeight":"3.6em"});
+$(".pass-body .head .dif h1").css({"lineHeight":"3.6em"});
 });
