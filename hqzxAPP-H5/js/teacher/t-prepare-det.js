1ef523adc6b065a126af800b5e8c364173108378
yanghao-000 <yanghao_000@126.com>
2016-11-01 17:13:57 +0800

    123
    123

--- hqzxAPP-H5/js/teacher/t-prepare-det.js
+++ hqzxAPP-H5/js/teacher/t-prepare-det.js
@@ -1,6 +1,7 @@
 $(function(){
 	slideHuaDong();
 	videoControl();
+	slideUpDown();
 });
 
 function videoControl(){
@@ -11,10 +12,28 @@ function videoControl(){
 		bg.stop().fadeOut(200);
 		video[0].play();
 		video.attr("controls","controls");
+		video.removeAttr("style");
 		screen.orientation.lock("landscape");
 	});
 	video[0].onpause = function(){
 		bg.stop().fadeIn(200);
 		video.removeAttr("controls");
 	};
+}
+
+function slideUpDown(){
+	var zhang = $(".box-out .box .box-zhang");
+	var jie = $(".box-out .box .box-jie");
+	var wrap = $(".swiper-wrapper");
+	zhang.on("click",function(){
+		if($(this).hasClass("act")){
+			$(this).removeClass("act");
+			wrap.css({"height":"auto"});
+			$(this).siblings(".box-jie").stop(true,true).slideUp(200);
+		}else{
+			$(this).addClass("act");
+			wrap.css({"height":"auto"});
+			$(this).siblings(".box-jie").stop(true,true).slideDown(200);
+		}
+	});
 }
