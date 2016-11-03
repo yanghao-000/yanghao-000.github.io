fcdf3769597b802861a19da5d63c3a34264d3e73
yanghao-000 <yanghao_000@126.com>
2016-11-03 09:25:12 +0800

    123
    123

--- hqzxAPP-H5/js/com/com-t.js
+++ hqzxAPP-H5/js/com/com-t.js
@@ -17,7 +17,7 @@ function judgeAndTeacher(){
 		$(".check-in-body ul li").each(function(){
 			$(this).find("i").eq(0).css({"marginTop":"25%"});
 		});
-		$(".slide-nav").css({"width":"126%"});
+		$(".slide-nav.slide-nav-scroll").css({"width":"126%"});
 		$(".course-list-box .item .right").each(function(){
 			$(this).find("p").eq(0).css({"marginBottom":"0.2em"});
 		});
