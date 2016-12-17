57977679dd99bd92d609cc4ba3d7ba6ad6712499
yanghao-000 <yanghao_000@126.com>
2016-12-17 13:58:10 +0800

    123
    123

--- kqjy-app-xuan/js/judge.js
+++ kqjy-app-xuan/js/judge.js
@@ -1,4 +1,4 @@
-//$(function(){
+$(function(){
 	var browser={
   	  	versions:function(){
             var u = navigator.userAgent, app = navigator.appVersion;
@@ -10,7 +10,8 @@
                 android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, 
                 iPhone: u.indexOf('iPhone') > -1 , //是否为iPhone或者QQHD浏览器
                 iPad: u.indexOf('iPad') > -1, //是否iPad
-                webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+                webApp: u.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
+                wx: window.navigator.userAgent.toLowerCase().match(/MicroMessenger/i) == 'micromessenger'
             };
          }(),
          language:(navigator.browserLanguage || navigator.language).toLowerCase()
@@ -19,12 +20,29 @@
 	$(".download-btn").on("click",function(){
 		download();
 	});
+	cssSet();
+	judgeWeb();
 	
 	function download(){
 		if(browser.versions.ios){
+			$(".tips-bar,.tips-bar-move").fadeIn();
 			window.location.href="https://itunes.apple.com/us/app/hui-da/id1064833923?l=zh&ls=1&mt=8";
 		}else if(browser.versions.android){
 			window.location.href="http://answerimg.kjcity.com/app/Answer_Student_v2.0.0.apk";
 		}
 	}
-//});
+	function judgeWeb(){
+		if(!browser.versions.wx){
+			download();
+		}
+	}
+	function cssSet(){
+		if(browser.versions.android){
+			$(".pad").css({"padding-top":"6em"});
+			$(".ani-1").css({"height":"34em"});
+			$(".ani-2").css({"height":"34em"});
+			$(".ani-3").css({"height":"43em"});
+			$(".ani-4").css({"height":"39em"});
+		}
+	}
+});
