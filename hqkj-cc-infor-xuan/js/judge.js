698dcf00b787f0971758c365b9230fe8afdb12d8
yanghao-000 <yanghao_000@126.com>
2016-12-30 13:55:54 +0800

    123
    123

--- hqkj-cc-infor-xuan/js/judge.js
+++ hqkj-cc-infor-xuan/js/judge.js
@@ -1,4 +1,7 @@
-$(function(){ 
+var id = "3F2147C49F6DF30F9C33DC5901307461";
+var h = "auto";
+document.getElementById("voide-box").src = "http://p.bokecc.com/player?vid=" + id + "&siteid=FE7A65E6BE2EA539&autoStart=true&width=100%&height=" + h + "&playerid=CED4B0511C5D4992&playertype=1";
+$(function(){
 	
 	var timer = setInterval(function(){
 		if($("video").length>0){
