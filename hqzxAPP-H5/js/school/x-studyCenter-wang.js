25a8df6ec4c2df52b5a25020a3be3e3bf0e1c3fd
yanghao-000 <yanghao_000@126.com>
2017-02-05 10:18:35 +0800

    123
    123

--- hqzxAPP-H5/js/school/x-studyCenter-wang.js
+++ hqzxAPP-H5/js/school/x-studyCenter-wang.js
@@ -2,15 +2,22 @@ $(function(){
 	learningLoading();
 	learningCenterZhan();
 	slideChannel();
+	wangShowPop();
 });
 
 //在线录播-在线直播切换
 function slideChannel(){
 	var box = $(".sec-lr-box-out .sec-lr-box");
 	
-	$(".sec-lr .t-i").on("click",function(){
+	$(".sec-lr .t-i").on("touchend",function(){
 		var i = $(this).index(".t-i");
 		$(this).addClass("act").siblings(".t-i").removeClass("act");
 		box.eq(i).stop(true,true).fadeIn(200).siblings().hide(0);
+	});
+}
+//网络课程-选择弹框
+function wangShowPop(){
+	$(".sec-lr-bar").on("touchend",function(){
+		choicePopAfter($(".choice-pop"),function(){});
 	});
 }
